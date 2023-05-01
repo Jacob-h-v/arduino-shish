9ac3d3d903c0e0bfa4d8f78b25c3bee73ced1d98
Jacob-h-v <92727932+Jacob-h-v@users.noreply.github.com>
2023-05-01 10:49:56 +0200

    added start/end of message protocol

--- PulseSensor_BPM.ino
+++ PulseSensor_BPM.ino
@@ -1,94 +1,36 @@
-
 #define USE_ARDUINO_INTERRUPTS true
 #include <PulseSensorPlayground.h>
 
-const int OUTPUT_TYPE = SERIAL_PLOTTER;
+//  Variables
+const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED = LED_BUILTIN;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 518;           // Determine which Signal to "count as a beat" and which to ignore.   
+int Signal;                           
 
-/*
-   Pinout:
-     PULSE_INPUT = Analog Input. Connected to the pulse sensor
-      purple (signal) wire.
-     PULSE_BLINK = digital Output. Connected to an LED (and 1K series resistor)
-      that will flash on each detected pulse.
-     PULSE_FADE = digital Output. PWM pin onnected to an LED (and 1K series resistor)
-      that will smoothly fade with each pulse.
-      NOTE: PULSE_FADE must be a pin that supports PWM. Do not use
-      pin 9 or 10, because those pins' PWM interferes with the sample timer.
-     THRESHOLD should be set higher than the PulseSensor signal idles
-      at when there is nothing touching it. The expected idle value
-      should be 512, which is 1/2 of the ADC range. To check the idle value
-      open a serial monitor and make note of the PulseSensor signal values
-      with nothing touching the sensor. THRESHOLD should be a value higher
-      than the range of idle noise by 25 to 50 or so. When the library
-      is finding heartbeats, the value is adjusted based on the pulse signal
-      waveform. THRESHOLD sets the default when there is no pulse present.
-      Adjust as neccesary.
-*/
-const int PULSE_INPUT = A0;
-const int PULSE_BLINK = LED_BUILTIN;
-const int PULSE_FADE = 5;
-const int THRESHOLD = 550;   // Adjust this number to avoid noise when idle
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
 
-PulseSensorPlayground pulseSensor;
+void setup() {        
+  Serial.begin(115200);     
+   
+  // Configure the PulseSensor object, by assigning our variables to it. 
+  pulseSensor.analogInput(PulseWire);
+  pulseSensor.blinkOnPulse(LED);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);
 
-void setup() {
-  /*
-     Use 115200 baud because that's what the Processing Sketch expects to read,
-     and because that speed provides about 11 bytes per millisecond.
-
-     If we used a slower baud rate, we'd likely write bytes faster than
-     they can be transmitted, which would mess up the timing
-     of readSensor() calls, which would make the pulse measurement
-     not work properly.
-  */
-  Serial.begin(115200);
-
-  // Configure the PulseSensor manager.
-
-  pulseSensor.analogInput(PULSE_INPUT);
-  pulseSensor.blinkOnPulse(PULSE_BLINK);
-  pulseSensor.fadeOnPulse(PULSE_FADE);
-
-  pulseSensor.setSerial(Serial);
-  pulseSensor.setOutputType(OUTPUT_TYPE);
-  pulseSensor.setThreshold(THRESHOLD);
-
-  // Now that everything is ready, start reading the PulseSensor signal.
-  if (!pulseSensor.begin()) {
-    /*
-       PulseSensor initialization failed,
-       likely because our particular Arduino platform interrupts
-       aren't supported yet.
-
-       If your Sketch hangs here, try PulseSensor_BPM_Alternative.ino,
-       which doesn't use interrupts.
-    */
-    for(;;) {
-      // Flash the led to show things didn't work.
-      digitalWrite(PULSE_BLINK, LOW);
-      delay(50);
-      digitalWrite(PULSE_BLINK, HIGH);
-      delay(50);
-    }
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
+   if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.   
   }
 }
 
 void loop() {
-  /*
-     Wait a bit.
-     We don't output every sample, because our baud rate
-     won't support that much I/O.
-  */
-  delay(20);
-
-  // write the latest sample to Serial.
- pulseSensor.outputSample();
-
-  /*
-     If a beat has happened since we last checked,
-     write the per-beat information to Serial.
-   */
-  if (pulseSensor.sawStartOfBeat()) {
-   pulseSensor.outputBeat();
+  
+  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+    int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+    Serial.print("<START>");
+    Serial.print(myBPM);
+    Serial.println("<END>");
   }
+   
+  delay(20);
 }
