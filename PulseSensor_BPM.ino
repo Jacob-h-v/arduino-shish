a5300bf1078d998ba13122bd095b4aeb727fbcff
Jacob-h-v <92727932+Jacob-h-v@users.noreply.github.com>
2023-05-09 10:39:48 +0200

    Update PulseSensor_BPM.ino

--- PulseSensor_BPM.ino
+++ PulseSensor_BPM.ino
@@ -5,7 +5,7 @@
 const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
 const int LED = LED_BUILTIN;          // The on-board Arduino LED, close to PIN 13.
 int Threshold = 510;           // Determine which Signal to "count as a beat" and which to ignore.   
-int Signal;                           
+int rawSignal;                       
 
 PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
 
@@ -19,18 +19,21 @@ void setup() {
 
   // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
    if (pulseSensor.begin()) {
-    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.   
+    //Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.   
   }
 }
 
 void loop() {
-  
-  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+  // Read the raw signal from pulse sensor
+  rawSignal = analogRead(0);
+  //if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
     int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
     Serial.print("<START>");
     Serial.print(myBPM);
+    Serial.print(",");
+    Serial.print(rawSignal - Threshold);
     Serial.println("<END>");
-  }
+  
    
-  delay(20);
+  delay(100);
 }
